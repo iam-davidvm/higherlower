6196a2fa48bce203eeddb0c136d2154cac5021dd
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-01-13 20:50:15 +0100

    JS for the index page

--- cards.js
+++ cards.js
@@ -12,9 +12,303 @@ export const CARDS = [
       img: '/2S.png'
     },
     {
+      value: 3,
+      card: '3S',
+      description: 'Three of Spades',
+      img: '/3S.png'
+    },
+    {
+      value: 4,
+      card: '4S',
+      description: 'Four of Spades',
+      img: '/4S.png'
+    },
+    {
+      value: 5,
+      card: '5S',
+      description: 'Five of Spades',
+      img: '/5S.png'
+    },
+    {
+      value: 6,
+      card: '6S',
+      description: 'Six of Spades',
+      img: '/6S.png'
+    },
+    {
+      value: 7,
+      card: '7S',
+      description: 'Seven of Spades',
+      img: '/7S.png'
+    },
+    {
+      value: 8,
+      card: '8S',
+      description: 'Eight of Spades',
+      img: '/8S.png'
+    },
+    {
+      value: 9,
+      card: '9S',
+      description: 'Nine of Spades',
+      img: '/9S.png'
+    },
+    {
       value: 10,
+      card: '10S',
+      description: 'Ten of Spades',
+      img: '/10S.png'
+    },
+    {
+      value: 11,
+      card: 'JS',
+      description: 'Jack of Spades',
+      img: '/JS.png'
+    },
+    {
+      value: 12,
+      card: 'QS',
+      description: 'Queen of Spades',
+      img: '/QS.png'
+    },
+    {
+      value: 13,
+      card: 'KS',
+      description: 'King of Spades',
+      img: '/KS.png'
+    },
+    {
+      value: 1,
+      card: 'AH',
+      description: 'Ace of Hearts',
+      img: '/AH.png'
+    },
+    {
+      value: 2,
+      card: '2H',
+      description: 'Two of Hearts',
+      img: '/2H.png'
+    },
+    {
+      value: 3,
+      card: '3H',
+      description: 'Three of Hearts',
+      img: '/3H.png'
+    },
+    {
+      value: 4,
+      card: '4H',
+      description: 'Four of Hearts',
+      img: '/4H.png'
+    },
+    {
+      value: 5,
+      card: '5H',
+      description: 'Five of Hearts',
+      img: '/5H.png'
+    },
+    {
+      value: 6,
+      card: '6H',
+      description: 'Six of Hearts',
+      img: '/6H.png'
+    },
+    {
+      value: 7,
+      card: '7H',
+      description: 'Seven of Hearts',
+      img: '/7H.png'
+    },
+    {
+      value: 8,
+      card: '8H',
+      description: 'Eight of Hearts',
+      img: '/8H.png'
+    },
+    {
+      value: 9,
+      card: '9H',
+      description: 'Nine of Hearts',
+      img: '/9H.png'
+    },
+    {
+      value: 10,
+      card: '10H',
+      description: 'Ten of Hearts',
+      img: '/10H.png'
+    },
+    {
+      value: 11,
+      card: 'JH',
+      description: 'Jack of Hearts',
+      img: '/JH.png'
+    },
+    {
+      value: 12,
+      card: 'QH',
+      description: 'Queen of Hearts',
+      img: '/QH.png'
+    },
+    {
+      value: 13,
+      card: 'KH',
+      description: 'King of Hearts',
+      img: '/KH.png'
+    },
+    {
+      value: 1,
+      card: 'AD',
+      description: 'Ace of Diamonds',
+      img: '/AD.png'
+    },
+    {
+      value: 2,
+      card: '2D',
+      description: 'Two of Diamonds',
+      img: '/2D.png'
+    },
+    {
+      value: 3,
+      card: '3D',
+      description: 'Three of Diamonds',
+      img: '/3D.png'
+    },
+    {
+      value: 4,
+      card: '4D',
+      description: 'Four of Diamonds',
+      img: '/4D.png'
+    },
+    {
+      value: 5,
+      card: '5D',
+      description: 'Five of Diamonds',
+      img: '/5D.png'
+    },
+    {
+      value: 6,
+      card: '6D',
+      description: 'Six of Diamonds',
+      img: '/6D.png'
+    },
+    {
+      value: 7,
+      card: '7D',
+      description: 'Seven of Diamonds',
+      img: '/7D.png'
+    },
+    {
+      value: 8,
+      card: '8D',
+      description: 'Eight of Diamonds',
+      img: '/8D.png'
+    },
+    {
+      value: 9,
+      card: '9D',
+      description: 'Nine of Diamonds',
+      img: '/9D.png'
+    },
+    {
+      value: 10,
+      card: '10D',
+      description: 'Ten of Diamonds',
+      img: '/10D.png'
+    },
+    {
+      value: 11,
       card: 'JD',
       description: 'Jack of Diamonds',
       img: '/JD.png'
-    }
+    },
+    {
+      value: 12,
+      card: 'QD',
+      description: 'Queen of Diamonds',
+      img: '/QD.png'
+    },
+    {
+      value: 13,
+      card: 'KD',
+      description: 'King of Diamonds',
+      img: '/KD.png'
+    },
+    {
+      value: 1,
+      card: 'AC',
+      description: 'Ace of Diamonds',
+      img: '/AC.png'
+    },
+    {
+      value: 2,
+      card: '2C',
+      description: 'Two of Diamonds',
+      img: '/2C.png'
+    },
+    {
+      value: 3,
+      card: '3C',
+      description: 'Three of Diamonds',
+      img: '/3C.png'
+    },
+    {
+      value: 4,
+      card: '4C',
+      description: 'Four of Diamonds',
+      img: '/4C.png'
+    },
+    {
+      value: 5,
+      card: '5C',
+      description: 'Five of Diamonds',
+      img: '/5C.png'
+    },
+    {
+      value: 6,
+      card: '6C',
+      description: 'Six of Diamonds',
+      img: '/6C.png'
+    },
+    {
+      value: 7,
+      card: '7C',
+      description: 'Seven of Diamonds',
+      img: '/7C.png'
+    },
+    {
+      value: 8,
+      card: '8C',
+      description: 'Eight of Diamonds',
+      img: '/8C.png'
+    },
+    {
+      value: 9,
+      card: '9C',
+      description: 'Nine of Diamonds',
+      img: '/9C.png'
+    },
+    {
+      value: 10,
+      card: '10C',
+      description: 'Ten of Diamonds',
+      img: '/10C.png'
+    },
+    {
+      value: 11,
+      card: 'JC',
+      description: 'Jack of Diamonds',
+      img: '/JC.png'
+    },
+    {
+      value: 12,
+      card: 'QC',
+      description: 'Queen of Diamonds',
+      img: '/QC.png'
+    },
+    {
+      value: 13,
+      card: 'KC',
+      description: 'King of Diamonds',
+      img: '/KC.png'
+    },
   ];
--- js/app.js
+++ js/app.js
@@ -1,2 +1,64 @@
+/*
+ *
+ * IMPORTING THE CARDS 
+ * 
+ */
 import { CARDS } from '../cards.js';
 const cardsObject = CARDS;
+
+/*
+ *
+ * HOMEPAGE
+ * 
+ */
+
+// Variables
+const continueGameBtn = document.querySelector('.btn-continue');
+const newGameBtn = document.querySelector('.btn-new');
+
+// localstarage items
+let gameInProgress = localStorage.getItem('gameInProgress');
+let decksSetting = localStorage.getItem('decks-setting');
+let cardsSetting = localStorage.getItem('cards-setting');
+
+const decksSelector = document.querySelector('#decks');
+const cardsSelector = document.querySelector('#cards');
+
+
+newGameBtn.addEventListener('click', function() {
+    startGame('new');
+});
+
+
+// Start a new/next game from the homepage
+function startGame(game) {
+    if (game === 'new') {
+        localStorage.setItem('currentStreak', '0');
+    }
+    localStorage.setItem('decks-setting', decksSelector.value);
+    localStorage.setItem('cards-setting', cardsSelector.value);
+    window.open('./pages/higherlower.html', '_self');
+}
+
+// initializer
+(function init() {
+    const pageURL = window.location.href;
+
+    if (pageURL.includes('index')) {        
+        if (gameInProgress) {
+            continueGameBtn.classList.remove('hide');
+        } else {
+            continueGameBtn.classList.add('hide');
+        }
+
+        if (decksSetting) {
+            decksSelector.value = decksSetting;
+        }
+
+        if (cardsSetting) {
+            cardsSelector.value = cardsSetting;
+        }
+    } else {
+        
+    }
+})();
